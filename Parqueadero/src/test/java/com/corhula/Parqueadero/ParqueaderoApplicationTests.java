79b6cc4e338639c8665878fa1fb44f9471a36262
Juan Esteban <oliverosduranj@gmail.com>
2025-03-20 07:31:22 -0500

    Backend

---
+++ Parqueadero/src/test/java/com/corhula/Parqueadero/ParqueaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhula.Parqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
